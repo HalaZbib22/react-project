b75780810b00775851abce61b38076c95030211a
Hala Zbib <halazbib22@gmail.com>
2022-03-01 00:58:57 +0200

    adding a landing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
-import Navbar from './components/parts/Navbar';
+// import Navbar from './components/parts/Navbar';
 import Home from './components/Home';
+import Landing from './components/Dashboard/Landing';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
       <div>
         <BrowserRouter>
-         <Navbar />
+          <Landing/>
           <Routes>
             <Route path='/home' exact element={<Home/>}/>
             <Route path='/signin' element={<SignIn/>}/>
