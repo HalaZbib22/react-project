2d4d73d134cc6aa57d6c1c653228ef6120a850a0
Hala Zbib <halazbib22@gmail.com>
2022-03-01 02:28:55 +0200

    adding a carousel

--- src/components/parts/Sidebar.js
+++ src/components/parts/Sidebar.js
@@ -13,31 +13,7 @@ function Sidebar(props) {
     <Grid item xs={12} md={12}>
 
       <div align="center">
-        {/* START OF CAROUSEL */}
 
-        {/* <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="img1">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="img2">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="img3">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div> */}
-
-        {/* END OF CAROUSEL */}
       <Typography variant="h6" gutterBottom sx={{ mt: 3 }}>
         Social
       </Typography>
